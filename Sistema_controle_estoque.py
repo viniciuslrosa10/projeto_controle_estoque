ab05c675abdb2bc6555bfa8da0d29d48e4f64df5
viniciuslrosa10 <vinicius.l.rosa@edu.senai.br>
2026-05-29 13:51:31 -0300

    Defini a variável produtos, e adicionei os produtos dentro da matriz

---
+++ Sistema_controle_estoque.py
@@ -0,0 +1,36 @@
+produtos = [
+    [100, "Martelo", 10, "M-01-01"],
+    [200, "Chave de Fenda", 5, "C-01-01"]
+    [210, "Chave"]
+]
+
+def adicionarprodutos():
+    novoProduto = input("Adicione os dados do novo produto: ") 
+    produtos.append(novoProduto) 
+    print("Produto inserido com sucesso! 📦")
+
+def listar_produtos():
+    for lista in produtos:
+        print(f"Os produtos disponíveis são: {lista}")
+
+def busca_produtos():
+
+        
+
+
+print("\nSistema de Controle de Estoque Simplificado (SCES)")
+
+while True:       
+    print("\n1- Adicionar novo produto | 2- Listar todos os produtos | 3- Buscar produto por ID | 4- Atualizar estoque | 5- Sair")
+    opcao = input("Escolha: ")
+    if (opcao == "1"):
+        adicionarprodutos()
+    elif (opcao == "2"):
+        viajar()
+    elif (opcao == "3"):
+        abastecer()
+    elif (opcao == "4"):
+        adicionarprodutos()
+    elif (opcao == "5"):
+        print("Viagem encerrada!")
+        break
